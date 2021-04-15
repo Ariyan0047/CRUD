ea837088f563da9fa0a581f10ab40a395dda652e
Ariyan0047 <aariyan0047@gmail.com>
2021-04-15 10:17:13 +0600

    header & footer

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+// HEADER SECTION
+include_once "./templates/views/header.php";
+// END HEADER SECTION
 
-<body>
+// FOOTER SECTION
+include_once "./templates/views/footer.php";
 
-</body>
-
-</html>
+// END FOOTER SECTION
+?>
